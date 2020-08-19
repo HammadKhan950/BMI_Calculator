ca13797fc2b9df60a660735fa137a2d898d64709
Hammad Khan <hammadkhan950.hk@gmail.com>
2020-08-19 21:56:31 +0530

    BMI Calculator App (Flutter App development Course)

---
+++ lib/constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: Color(0xFF8D8E98),
+);
+const kBottomContainerHeight = 60.0;
+const kColorResusableCard = Color(0xFF1D1E33);
+const kBottomdawerColor = Color(0xFFEB1555);
+const kInactiveCardColor = Color(0xFF111328);
+
+const kNumberText = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.w900,
+);
+const kLargeButtonTextStyle =
+TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+const kTitleTextStyle = TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold);
+const kResultTextStye = TextStyle(
+    color: Color(0xFF24D876), fontSize: 18.0, fontWeight: FontWeight.bold);
+const kBMITextStyle=TextStyle(
+    fontSize: 80.0,
+    fontWeight: FontWeight.bold
+);
+const kBodyTextStyle=TextStyle(
+  fontSize: 18.0,
+);
